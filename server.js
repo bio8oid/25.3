1201aefbb4bb92d653ffd8e95d215cfa85550963
bio8oid <8bollod8@mail.co.uk>
2019-07-04 01:59:36 +0200

    Add server.js and test.json files

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var stringifyFile;
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+
+    fs.readFile('./test.json',
+        'utf8',
+        function (err, data) {
+            if (err) throw err;
+            stringifyFile = data
+            res.send(data);
+        })
+});
+
+
+
+app.post('/updateNote/:note', function (req, res) {
+    console.log(+ req.params.note);
+
+    fs.writeFile('./test.json',
+        stringifyFile + req.params.note,
+        function (err) {
+            if (err) throw err;
+            console.log('file updated');
+        })
+});
+
+app.listen(3000);
