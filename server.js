82e58e4f347a8fc7eeb13ef67a01589f77ba6cf9
bio8oid <8bollod8@mail.co.uk>
2019-11-19 23:53:35 +0100

    Refactoring

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
-var stringifyFile;
-var bodyParser = require('body-parser')
+import 'express';
+const app = express();
+const fs = require('fs');
+const stringifyFile;
+const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function (req, res) {
+app.get('/getNote', (req, res) => {
 
     fs.readFile('./test.json',
         'utf8',
-        function (err, data) {
+        (err, data) => {
             if (err) throw err;
             stringifyFile = data
             res.send(data);
         })
 });
 
-app.post('/updateNote/:note', function (req, res) {
+app.post('/updateNote/:note', (req, res) => {
 
     fs.writeFile('./test.json',
         stringifyFile + req.params.note,
-        function (err) {
+        (err) => {
             if (err) throw err;
             console.log('file updated');
         })
 });
 
-app.listen(3000);
+app.listen(port, () => console.log(`listening @ ${port}`));
